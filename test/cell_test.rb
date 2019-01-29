5987854980e33d009c6ed37a829c4c724ab8dc81
Jon Peterson <jon@pnwebs.com>
2019-01-29 16:31:47 -0700

    Add Cell and more tests

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -7,4 +7,8 @@ class CellTest < Minitest::Test
   def test_cell_exists
     assert_instance_of Cell, @cell
   end
+  def test_cell_default_attributes
+    assert_equal "B4", @cell.coordinate
+    assert_nil @cell.ship
+  end
 end
