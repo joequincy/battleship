c53b3b6564248bf2a9dfc7885e0fb67e31d673bc
Stella Mainar <mainars09@alumni.hanover.edu>
2019-01-29 17:03:42 -0700

    Add more tests for Cell

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -3,6 +3,7 @@ require './test/test_helper'
 class CellTest < Minitest::Test
   def setup
     @cell = Cell.new("B4")
+    @cruiser = Ship.new("Cruiser", 3)
   end
   def test_cell_exists
     assert_instance_of Cell, @cell
@@ -11,4 +12,16 @@ class CellTest < Minitest::Test
     assert_equal "B4", @cell.coordinate
     assert_nil @cell.ship
   end
+  def test_cell_is_empty_by_default
+    assert @cell.empty?
+  end
+  def test_cell_can_place_ship
+    @cell.place_ship(@cruiser)
+    assert_equal @cruiser, @cell.ship
+  end
+  def test_cell_not_empty_if_ship_is_placed
+    @cell.place_ship(@cruiser)
+    refute @cell.empty?
+  end
+  
 end
