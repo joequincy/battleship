7ab2d6746e82b47816538101bb34f342d8856004
Jon Peterson <jon@pnwebs.com>
2019-02-03 21:02:39 -0700

    Adds ability to accept --color command line argument, prints board in color.

--- lib/cell.rb
+++ lib/cell.rb
@@ -3,6 +3,9 @@ require './lib/ship'
 class Cell
   attr_reader :coordinate, :ship
 
+  @@colorize = (ARGV.length > 0 && ARGV[0] == "--color") ? true : false
+  ## if we start the runner with a --color switch, render in color.
+  
   def initialize(coordinate)
     @coordinate = coordinate
     @ship = nil
@@ -27,18 +30,36 @@ class Cell
   end
 
   def render(show_all_ships = false)
-    if fired_upon? && @ship
-      if @ship.sunk?
-        "X"
+    if @@colorize
+      if fired_upon? && @ship
+        if @ship.sunk?
+          "\e[90mX\e[0m"
+        else
+          "\e[31mH\e[0m"
+        end
+      elsif fired_upon?
+        "\e[93mM\e[0m"
+      elsif @ship && show_all_ships
+        "S"
       else
-        "H"
+        "\e[36m\u2592\e[0m"
       end
-    elsif fired_upon?
-      "M"
-    elsif @ship && show_all_ships
-      "S"
     else
-      "."
+      if fired_upon? && @ship
+        if @ship.sunk?
+          "X"
+        else
+          "H"
+        end
+      elsif fired_upon?
+        "M"
+      elsif @ship && show_all_ships
+        "S"
+      else
+        "."
+      end
     end
   end
 end
+
+ARGV.clear
