ba88a776a0646b25880f3ff21d280f7eb9078851
Jon Peterson <jon@pnwebs.com>
2019-02-03 20:38:37 -0700

    Add more tests for Computer.

--- test/computer_test.rb
+++ test/computer_test.rb
@@ -18,8 +18,6 @@ class ComputerTest < Minitest::Test
     spaces_with_ships = board.cells.count do |key,cell|
       cell.empty? == false
     end
-    # ships array should change, and number of !empty? cells
-    # should be equal to the number of spaces it was placing
     assert_equal 2, response.length
 
     assert_equal 5, spaces_with_ships
@@ -38,10 +36,33 @@ class ComputerTest < Minitest::Test
     spaces_with_ships = board.cells.count do |key,cell|
       cell.empty? == false
     end
-    # ships array should change, and number of !empty? cells
-    # should be equal to the number of spaces it was placing
     assert_equal 5, response.length
 
     assert_equal 17, spaces_with_ships
+  end
+
+  def test_computer_can_limit_starting_coords
+    default_board = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
+    limited_coords_3 = @computer.limit_coords(default_board, 3)
+    limited_coords_2 = @computer.limit_coords(default_board, 2)
+    assert_equal ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "D1", "D2"], limited_coords_3
+
+    assert_equal ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3"], limited_coords_2
+  end
+
+  def test_computer_can_get_consecutive_coords
+    result = @computer.get_consecutive_coords("A2", 3)
+    assert_equal ["A2", "A3", "A4"], result[:horizontal]
+
+    assert_equal ["A2", "B2", "C2"], result[:vertical]
+  end
+
+  def test_computer_can_take_shot
+    message = @computer.take_shot
+    # not really sure how to test this. Nothing should be exposed as
+    # an attr_reader, and I want to pass back a message and some
+    # attributes to give feedback to the player.
+    refute message[:sunk]
+    assert_nil message[:ship]
   end
 end
